bd303b371fceb55f58fb22bca1ceffc64bd23e1e
tafsirony <onykhan95@gmail.com>
2020-07-24 16:51:52 +0600

    header & footer component added

---
+++ intoReact/confusion/src/components/HeaderComponent.js
@@ -0,0 +1,31 @@
+import React, { Component } from "react";
+import { Navbar, NavbarBrand, Jumbotron } from "reactstrap";
+class Header extends Component {
+  render() {
+    return (
+      <>
+        <Navbar dark>
+          <div className="container">
+            <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
+          </div>
+        </Navbar>
+        <Jumbotron>
+          <div className="container">
+            <div className="row row-header">
+              <div className="col-12 col-sm-6">
+                <h1>Ristorante Con Fusion</h1>
+                <p>
+                  We take inspiration from the world's nesy cuisines, and create
+                  a unique fusion expereince.Our lipsmacking creations will
+                  tickle your taste.
+                </p>
+              </div>
+            </div>
+          </div>
+        </Jumbotron>
+      </>
+    );
+  }
+}
+
+export default Header;
