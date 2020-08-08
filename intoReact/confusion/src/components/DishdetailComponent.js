b66b43d16fdc697819c833ee599b25dc5bc01639
tafsirony <onykhan95@gmail.com>
2020-08-08 22:23:44 +0600

    React Animation Components

--- intoReact/confusion/src/components/DishdetailComponent.js
+++ intoReact/confusion/src/components/DishdetailComponent.js
@@ -19,6 +19,7 @@ import { Link } from "react-router-dom";
 import { Control, LocalForm, Errors } from "react-redux-form";
 import { Loading } from "./LoadingComponent";
 import { baseUrl } from "../shared/baseUrl";
+import { FadeTransform, Fade, Stagger } from "react-animation-components";
 
 const required = (val) => val && val.length;
 const maxLength = (len) => (val) => !val || val.length <= len;
@@ -26,13 +27,20 @@ const minLength = (len) => (val) => val && val.length >= len;
 
 function RenderDish({ dish }) {
   return (
-    <Card>
-      <CardImg width="100%" src={baseUrl + dish.image} alt={dish.name} />
-      <CardBody>
-        <CardTitle heading>{dish.name}</CardTitle>
-        <CardText>{dish.description}</CardText>
-      </CardBody>
-    </Card>
+    <FadeTransform
+      in
+      transformProps={{
+        exitTransform: "scale(0.5) translateY(-50%)",
+      }}
+    >
+      <Card>
+        <CardImg width="100%" src={baseUrl + dish.image} alt={dish.name} />
+        <CardBody>
+          <CardTitle heading>{dish.name}</CardTitle>
+          <CardText>{dish.description}</CardText>
+        </CardBody>
+      </Card>
+    </FadeTransform>
   );
 }
 
@@ -49,10 +57,12 @@ function RenderComments({ comments, postComment, dishId }) {
 
       return (
         <ul key={cmnts.id} className="list-unstyled">
-          <li className="comment">{cmnts.comment}</li>
-          <li className="author">
-            -- {cmnts.author}, {date}
-          </li>
+          <Fade in>
+            <li className="comment">{cmnts.comment}</li>
+            <li className="author">
+              -- {cmnts.author}, {date}
+            </li>
+          </Fade>
         </ul>
       );
     });
