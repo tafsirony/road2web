a5e3105cf46b48f2e5ddad8c284d2c1e10a77164
tafsirony <onykhan95@gmail.com>
2020-08-09 21:03:27 +0600

    assignment 4 task 2

--- intoReact/confusion/src/components/AboutComponent.js
+++ intoReact/confusion/src/components/AboutComponent.js
@@ -10,6 +10,7 @@ import {
 import { Link } from "react-router-dom";
 import { Loading } from "./LoadingComponent";
 import { baseUrl } from "../shared/baseUrl";
+import { FadeTransform, Fade, Stagger } from "react-animation-components";
 
 function RenderLeader({ leader }) {
   return (
@@ -128,7 +129,18 @@ function About(props) {
             <h2>Corporate Leadership</h2>
           </div>
           <div className="col-12 mt-5">
-            <Media list>{leaders}</Media>
+            {/* <Media list>{leaders}</Media> */}
+            <Stagger in>
+              {props.leaders.leaders.map((leader) => {
+                return (
+                  <Fade in>
+                    <div key={leader.id} className="col-12 m-1">
+                      <RenderLeader leader={leader} />
+                    </div>
+                  </Fade>
+                );
+              })}
+            </Stagger>
           </div>
         </div>
       </div>
